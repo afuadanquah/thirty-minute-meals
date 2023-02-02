d26173cf2aff78f43712b274561584b05fa49b7d
Afua <afua.danquah@sky.uk>
2023-02-02 12:54:13 +0000

    Added assertions to the NFT - check that all requests are successful are equal or greater than 99 percentile and check the max response time is less than and equal to 3000 milliseconds (3 seconds)

--- nft/src/main/scala/BasicSimulation.scala
+++ nft/src/main/scala/BasicSimulation.scala
@@ -18,14 +18,14 @@ class BasicSimulation extends Simulation {
 
   setUp(
     scn.inject(
-      constantUsersPerSec(10).during(5))).throttle(
+      constantUsersPerSec(10).during(5 minutes))).throttle(
     reachRps(10) in (30 seconds),
     holdFor(4.5 minutes),
     reachRps(0) in (30 seconds)
   ).protocols(httpProtocol)
     .assertions(
       global.successfulRequests.percent.gte(99),
-      global.responseTime.max.lte(3000)
+      global.responseTime.max.lte(3000) //3000 milliseconds (3 seconds) is 1 percent of 5 mind
     )
 
 
